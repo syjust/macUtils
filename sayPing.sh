2cd67f55c26b11d95112c331fd64b9c4841fda2d
Sylvain Just <sylvain.just.external@watchever.com>
2017-06-08 12:25:23 +0200

    sayPing choose audio device

--- sayPing.sh
+++ sayPing.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
-cnt=0
-host=10.31.100.206
-while true ; do
-  d="`date "+%Y-%m-%d %H:%M:%S"`"
-  echo -n "[$d] : ping $host ($cnt) ... "
-  ping -c 1 -i 1 $host > /dev/null 2>&1 \
-    && echo "OK" \
-    && break \
-    || echo "KO"
-  sleep 1
-  let cnt++
+export COLUMNS=30
+# speak when host comeback
+
+speak() {
+  echo "$@"
+  case $dev in
+    none)
+      say "$@"
+    ;;
+    silent)
+      echo "SILENT: $@"
+    ;;
+    *)
+      id=${dev%%_*}
+      #echo "id:$id"
+      say -a $id "$@"
+    ;;
+  esac
+}
+
+function quit() {
+  speak "ERROR: $@"
+  exit 1
+}
+
+host=$1
+device_list=`say -a ? | sed 's/ /_/g;s/^_*//g'`
+echo "choose device output : "
+select dev in none silent $device_list ; do
+  echo "'$dev' choosen"
+  break
 done
 
-say -a 46 "syl dev host came back"
+if [ -z "$host" ] ; then
+  quit "host not set"
+else
+  cnt=0
+  while true ; do
+    d="`date "+%Y-%m-%d %H:%M:%S"`"
+    echo -n "[$d] : ping $host ($cnt) ... "
+    ping -c 1 -i 1 $host > /dev/null 2>&1 \
+      && echo "OK" \
+      && break \
+      || echo "KO"
+    sleep 1
+    let cnt++
+  done
+  speak "host $host came back"
+fi
+
